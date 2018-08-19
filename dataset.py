b3f4629b94b99cc96794a7bdfc2e96dfbcb01f14
dimitreandrew@gmail.com <dimitreandrew@gmail.com>
2018-08-19 00:08:37 -0300

    added tensorflow dynamic feature engineering

--- dataset.py
+++ dataset.py
@@ -1,3 +1,4 @@
+import numpy as np
 import tensorflow as tf
 
 
@@ -7,7 +8,7 @@ def read_dataset(filename, mode, features_cols, label_col, default_value, batch_
             columns = tf.decode_csv(value_column, record_defaults=default_value)
             features = dict(zip(features_cols, columns))
             label = features.pop(label_col)
-            return features, label
+            return add_engineered(features), label
 
         # Create list of file names that match "glob" pattern (i.e. data_file_*.csv)
         filenames_dataset = tf.data.Dataset.list_files(filename)
@@ -27,24 +28,25 @@ def read_dataset(filename, mode, features_cols, label_col, default_value, batch_
             num_epochs = 1  # end-of-input after this
 
         dataset = dataset.repeat(num_epochs).batch(batch_size)
+        batch_features, batch_labels = dataset.make_one_shot_iterator().get_next()
 
-        return dataset.make_one_shot_iterator().get_next()
+        return batch_features, batch_labels
 
     return _input_fn
 
 
 def get_train(data_path, features, label, default_value, batch_size=512):
-    return read_dataset(data_path, mode=tf.estimator.ModeKeys.TRAIN, features_cols=add_engineered(features), label_col=label,
+    return read_dataset(data_path, mode=tf.estimator.ModeKeys.TRAIN, features_cols=features, label_col=label,
                         default_value=default_value, batch_size=batch_size)
 
 
 def get_valid(data_path, features, label, default_value, batch_size=512):
-    return read_dataset(data_path, mode=tf.estimator.ModeKeys.EVAL, features_cols=add_engineered(features), label_col=label,
+    return read_dataset(data_path, mode=tf.estimator.ModeKeys.EVAL, features_cols=features, label_col=label,
                         default_value=default_value, batch_size=batch_size)
 
 
 def get_test(data_path, features, label, default_value, batch_size=512):
-    return read_dataset(data_path, mode=tf.estimator.ModeKeys.PREDICT, features_cols=add_engineered(features), label_col=label,
+    return read_dataset(data_path, mode=tf.estimator.ModeKeys.PREDICT, features_cols=features, label_col=label,
                         default_value=default_value, batch_size=batch_size)
 
 
@@ -52,9 +54,79 @@ def add_more_features(features):
     # Nothing to add (yet!)
     return features
 
+
 def add_engineered(features):
-    # Nothing to add (yet!)
+    # Feature engineering as data is feed
+    lat1 = features['pickup_latitude']
+    lat2 = features['dropoff_latitude']
+    lon1 = features['pickup_longitude']
+    lon2 = features['dropoff_latitude']
+    latdiff = (lat1 - lat2)
+    londiff = (lon1 - lon2)
+    euclidean = (latdiff ** 2 + londiff ** 2) ** 0.5
+
+    # Add new features
+    features['latdiff'] = latdiff
+    features['londiff'] = londiff
+    features['euclidean'] = euclidean
+
     return features
+
+
+def build_estimator(model_dir, nbuckets, hidden_units, input_columns):
+    # Input columns
+    (latdiff, londiff, euclidean, plon, plat, dlon, dlat, pcount) = input_columns
+
+    # Bucketize the lats & lons
+    latbuckets = np.linspace(38.0, 42.0, nbuckets).tolist()
+    lonbuckets = np.linspace(-76.0, -72.0, nbuckets).tolist()
+    b_plat = tf.feature_column.bucketized_column(plat, latbuckets)
+    b_dlat = tf.feature_column.bucketized_column(dlat, latbuckets)
+    b_plon = tf.feature_column.bucketized_column(plon, lonbuckets)
+    b_dlon = tf.feature_column.bucketized_column(dlon, lonbuckets)
+
+    # Feature cross
+    ploc = tf.feature_column.crossed_column([b_plat, b_plon], nbuckets * nbuckets)
+    dloc = tf.feature_column.crossed_column([b_dlat, b_dlon], nbuckets * nbuckets)
+    pd_pair = tf.feature_column.crossed_column([ploc, dloc], nbuckets ** 4)
+
+    # Wide columns and deep columns
+    wide_columns = [
+        # Feature crosses
+        dloc, ploc, pd_pair,
+
+        # Sparse columns
+
+        # Anythin with a linear relationship
+        pcount
+    ]
+
+    deep_columns = [
+        # Embedding_column to "group" together
+        tf.feature_column.embedding_column(pd_pair, 10),
+
+        # Numeric columns
+        plat, plon, dlat, dlon,
+        latdiff, londiff, euclidean
+    ]
+
+    estimator = tf.estimator.DNNLinearCombinedRegressor(
+        model_dir=model_dir,
+        linear_feature_columns=wide_columns,
+        dnn_feature_columns=deep_columns,
+        dnn_hidden_units=hidden_units)
+
+    # add extra evaluation metric for hyperparameter tuning
+    estimator = tf.contrib.estimator.add_metrics(estimator, add_eval_metrics)
+
+    return estimator
+
+
+def add_eval_metrics(labels, predictions):
+    pred_values = predictions['predictions']
+    return {
+        'rmse': tf.metrics.root_mean_squared_error(labels, pred_values)
+    }
 
 
 def pandas_train_input_fn(df, label):
